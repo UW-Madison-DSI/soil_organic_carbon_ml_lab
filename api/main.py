880f766c4196ce1cbd284b23e5a7e8f775db815c
Maria Oros <maria.oros1201@gmail.com>
2024-05-27 22:27:26 -0500

    FIX: pickle model call

--- api/main.py
+++ api/main.py
@@ -1,14 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+import joblib
+from sklearn.pipeline import Pipeline
 from pydantic import BaseModel
 from pandas import DataFrame
-import os
-from io import BytesIO
-from joblib import load
-import logging
-from sklearn.pipeline import Pipeline
-import joblib
 
-logging.basicConfig(level=logging.DEBUG)
 
 class PredictionRequest(BaseModel):
     Depth: float
@@ -27,43 +22,37 @@ class PredictionRequest(BaseModel):
     mrvbf: float
 
 class PredictionResponse(BaseModel):
-    socs: float
-
-def transform_to_dataframe(class_model: BaseModel) -> DataFrame:
-    """
-    Input: Patient data as JSON
-    Output: Dataframe with the same info
-    """
-    features = ['Depth', 'tmax', 'tmin', 'prcp', 'lc', 'clay', 'silt', 'sand', 'dem', 'slope', 'aspect', 'hillshade', 'twi', 'mrvbf']
-    dictionary = {key: [value] for key, value in class_model.dict().items()}
-    df = DataFrame(dictionary)
-    df = df[features]
-    return df
+    soil_organic_carbon_stock: float
 
 def get_model() -> Pipeline:
     """
     Input: model path
     Output: model
     """
-    rf_model = load('rf_model.joblib')
-    return rf_model
+    loaded_rf = joblib.load("rf_model.joblib")
+    return loaded_rf
 
-def get_prediction(request: PredictionRequest) -> float:
+def transform_to_dataframe(class_model: BaseModel) -> DataFrame:
+    """
+    Input: data as JSON
+    Output: Dataframe with the same info
+    """
+    dictionary = {key: [value] for key, value in class_model.dict().items()}
+    df = DataFrame(dictionary)
+    return df
+
+def get_prediction(request: PredictionRequest) -> str:
     """
     Input: request. PredictionRequest instance used to compute the prediction of the model
     Output: prediction. Prediction of the model
     """
     try:
         data_to_predict = transform_to_dataframe(request)
-
-        model = get_model()
-
-        prediction = model.predict(data_to_predict)[0]
-        return prediction
-        
+        loaded_rf = get_model()
+        predict = loaded_rf.predict(data_to_predict)
+        return predict[0]
     except Exception as e:
-        return str(e)
-
+        return -99.0
 
 app = FastAPI(docs_url='/')
 
@@ -71,6 +60,6 @@ app = FastAPI(docs_url='/')
 def make_model_prediction(request: PredictionRequest):
     """
     Input: Prediction request
-    Output: Prediction response. The prediction is 0 for Benign samples and 1 for Malignant samples, so we return the name instead the number
+    Output: Prediction response.
     """
-    return PredictionResponse(socs=get_prediction(request))
+    return PredictionResponse(soil_organic_carbon_stock=get_prediction(request))
