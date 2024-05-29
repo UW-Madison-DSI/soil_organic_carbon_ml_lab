e34d43824294b67a3efdb81810a111cb45cedfef
Maria Oros <maria.oros1201@gmail.com>
2024-05-29 10:24:59 -0500

    UPGRADE sh for deployment config

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,7 @@
-rsconnect deploy fastapi --server https://connect.doit.wisc.edu/ --api-key XXXXXXX ./
+if [ -z "$RSCONNECT_API_KEY" ]; then
+  echo "Error: RSCONNECT_API_KEY is not set."
+  exit 1
+fi
+
+# Deploy using the environment variable for the API key
+rsconnect deploy fastapi --server https://connect.doit.wisc.edu/ --api-key $RSCONNECT_API_KEY ./api_socs_forecast/
