ecac625ddf9f1c1f736bec3b23c05d36b77c5591
Maria Oros <maria.oros1201@gmail.com>
2024-07-30 12:19:45 -0500

    FIX server name

--- scripts/deploy_wisc.sh
+++ scripts/deploy_wisc.sh
@@ -8,8 +8,11 @@ echo "Starting deployment script..."
 echo "Installing rsconnect-python..."
 pip install -U rsconnect-python
 
-# Deploy the Streamlit app
+echo "Configuring Posit Connect server..."
+rsconnect add --server https://connect.doit.wisc.edu --name sdp_CONUS --api-key $POSIT_API_KEY
+
+# Deploy the Streamlit app with the specified name and entry point
 echo "Deploying the Streamlit app..."
-rsconnect deploy streamlit --server https://connect.doit.wisc.edu --name sdp_CONUS --api-key $POSIT_API_KEY --entrypoint app.py . --no-verify
+rsconnect deploy streamlit --name sdp_CONUS --entrypoint app.py .
 
 echo "Deployment script completed successfully."
