502572ff14b1af046cbfb1b07541e2c381662020
Maria Oros <maria.oros1201@gmail.com>
2024-07-30 11:56:05 -0500

    ADD deployment environment

--- scripts/deploy_wisc.sh
+++ scripts/deploy_wisc.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# Install rsconnect-python
 pip install -U rsconnect-python
+
+# Add Posit Connect server
 rsconnect add -i --server https://connect.doit.wisc.edu --name sdp_forecasting --api-key $POSIT_API_KEY
+
+# Deploy the Streamlit app
 rsconnect deploy streamlit -n wisc --entrypoint app.py .
