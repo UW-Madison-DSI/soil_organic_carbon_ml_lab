ac2f77f9e40217e05a2dce0459e03edc796458de
Maria Oros <maria.oros1201@gmail.com>
2024-10-10 20:07:30 -0500

    ADD fisrt files

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-if [ -z "$POSIT_API_KEY" ]; then
-  echo "Error: $POSIT_API_KEY is not set."
-  exit 1
-fi
-
-pip install -U rsconnect-python
-
-rsconnect add -i --server https://connect.doit.wisc.edu --name sdp_CONUS --api-key $POSIT_API_KEY
-rsconnect deploy streamlit -n sdp_CONUS --entrypoint app.py ./
